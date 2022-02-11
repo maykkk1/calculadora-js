b5566b29f9d3951f9ed982703b343b98ea32ceb7
maycon <maycon5591@gmail.com>
2022-02-11 14:32:58 -0300

    projeto terminado

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 let numberChoser = document.getElementById("result")
 let mathExpressionDisplay = document.getElementById("calc")
 let mathExpression = []
+let resetCalc = false
+let negativeNumber = false
+
+
 function addNumber (number){
+    if (resetCalc) {
+        numberChoser.innerHTML = ''
+        resetCalc = false
+    }
+    if (number === 'signs') {
+        if (!negativeNumber) {
+            auxString = numberChoser.innerHTML
+            numberChoser.innerHTML = '-'
+            numberChoser.innerHTML += auxString
+            negativeNumber = true
+            return
+        } else {
+            numberChoser.innerHTML = numberChoser.innerHTML.slice(1, numberChoser.innerHTML.length + 1)
+            negativeNumber = false
+            return
+        }
+    } 
     if (number === 'delete') {
         let erasedNumber = numberChoser.innerHTML
         numberChoser.innerHTML = erasedNumber.slice(0, -1)
         return
     }
-    if (numberChoser.innerHTML.length > 11) {
-        numberChoser.style.fontSize = '2rem'
+    if (numberChoser.innerHTML.length > 13) {
+        numberChoser.style.fontSize = '1.555rem'
     } 
-    if(numberChoser.innerHTML.length < 15) numberChoser.innerHTML += number
+    if(numberChoser.innerHTML.length < 18) numberChoser.innerHTML += number
 }
 
 
 function addOperation (operation) {
-    if (operation == 'C'){
+    if (operation == 'clear'){
         numberChoser.innerHTML = ''
         mathExpression = []
         mathExpressionDisplay.innerHTML = ''
+        negativeNumber = false
+        return
+    }
+    if (operation == 'clear entry') {
+        numberChoser.innerHTML = ''
         return
     }  
     if(numberChoser.innerHTML.length > 0) {
@@ -34,7 +60,7 @@ function addOperation (operation) {
 }
 
 
-function teste (){
+function calculate (){
     mathExpression.push(numberChoser.innerHTML);
     let elements = ''
     mathExpression.forEach(element => {
@@ -48,10 +74,8 @@ function teste (){
     numberChoser.innerHTML = res;
     mathExpression = [];
     mathExpressionDisplay.innerHTML = '';
+    resetCalc = true
 }
-
-
-
 
 
 
